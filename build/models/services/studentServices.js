61c70c870889965b1c1cc7957980581fb2ee50c4
LAPTOP-UTR8DH1I\MetaM <riocenit@gmail.com>
2022-11-26 22:39:10 +0100

    ranking ordenado de mayor a menor

--- build/models/services/studentServices.js
+++ build/models/services/studentServices.js
@@ -1,22 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.putOneStudent = exports.deleteOneStudent = exports.findOneStudent = exports.findRankingStudents = exports.createStudent = void 0;
+exports.findRankingStudents = void 0;
 const config_js_1 = require("../../config.js");
-function createStudent(student, callback) {
-    const queryString = "INSERT INTO student (name, first_surname, second_surname, email_personal, email_activa, phone_number, zip_code) VALUES (?, ?, ?, ?, ?, ?, ?)";
-    config_js_1.db.query(queryString, [student.name, student.firstSurname, student.secondSurname, student.personalEmailAddress, student.activaEmailAddress, student.phoneNumber, student.zipCode], (err, result) => {
-        if (err) {
-            callback(err, null);
-        }
-        ;
-        const insertId = result.insertId;
-        callback(null, insertId);
-    });
-}
-exports.createStudent = createStudent;
-;
 function findRankingStudents(callback) {
-    const queryString = "SELECT id, name, first_surname, second_surname, email_personal, email_activa, phone_number, zip_code FROM student";
+    const queryString = "SELECT name, first_surname, second_surname,activa_points_balance FROM student ORDER BY activa_points_balance DESC ";
     config_js_1.db.query(queryString, (err, result) => {
         if (err)
             callback(err, null);
@@ -25,39 +12,3 @@ function findRankingStudents(callback) {
     });
 }
 exports.findRankingStudents = findRankingStudents;
-function findOneStudent(id, callback) {
-    const queryString = "SELECT name, first_surname, second_surname, activa_points_balance FROM student";
-    config_js_1.db.query(queryString, [id], (err, result) => {
-        if (err) {
-            callback(err, null);
-        }
-        ;
-        const studentFound = result[0];
-        callback(null, studentFound);
-    });
-}
-exports.findOneStudent = findOneStudent;
-function deleteOneStudent(id, callback) {
-    const queryString = "DELETE FROM student WHERE id = ?";
-    config_js_1.db.query(queryString, [id], (err, result) => {
-        if (err) {
-            callback(err, null);
-        }
-        ;
-        const studentDeleted = "deleted succesfull";
-        callback(null, studentDeleted);
-    });
-}
-exports.deleteOneStudent = deleteOneStudent;
-function putOneStudent(id, student, callback) {
-    const queryString = "UPDATE student SET name=?, first_surname=?, second_surname=?, email_personal=?, email_activa=?, phone_number=?, zip_code=? WHERE id=?";
-    config_js_1.db.query(queryString, [student.name, student.firstSurname, student.secondSurname, student.personalEmailAddress, student.activaEmailAddress, student.phoneNumber, student.zipCode, id], (err, result) => {
-        if (err) {
-            callback(err, null);
-        }
-        ;
-        const studentUpdated = "update succesfull";
-        callback(null, studentUpdated);
-    });
-}
-exports.putOneStudent = putOneStudent;
